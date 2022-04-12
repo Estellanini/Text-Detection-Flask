2d6a3f5b5a7086121d7f9b244aeb5130aae18830
Estellanini <1357436238@qq.com>
2022-04-12 10:28:45 +0800

    feat: 新增敏感词分类模块编辑功能

--- applications/view/admin/wordkinds.py
+++ applications/view/admin/wordkinds.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from sqlalchemy import desc
 
 from applications.common import curd
-from applications.common.curd import model_to_dicts, enable_status, disable_status
+from applications.common.curd import model_to_dicts, enable_status, disable_status,get_one_by_id
 from applications.common.helper import ModelFilter
 from applications.common.utils.http import table_api, fail_api, success_api
 from applications.common.utils.rights import authorize
@@ -42,7 +42,7 @@ def data():
     return table_api(data=model_to_dicts(schema=WordkindsSchema, data=wordkinds.items), count=count)
 
 
-# 用户增加
+# 敏感词分类增加
 @admin_wordkinds.get('/add')
 @authorize("admin:wordkinds:add", log=True)
 def add():
@@ -89,41 +89,37 @@ def save():
 #     return success_api(msg="删除成功")
 #
 #
-# #  编辑用户
-# @admin_wordkinds.get('/edit/<int:id>')
-# @authorize("admin:user:edit", log=True)
-# def edit(id):
-#     user = curd.get_one_by_id(User, id)
-#     roles = Role.query.all()
-#     checked_roles = []
-#     for r in user.role:
-#         checked_roles.append(r.id)
-#     return render_template('admin/user/edit.html', user=user, roles=roles, checked_roles=checked_roles)
-#
-#
-# #  编辑用户
-# @admin_wordkinds.put('/update')
-# @authorize("admin:user:edit", log=True)
-# def update():
-#     req_json = request.json
-#     a = xss_escape(req_json.get("roleIds"))
-#     id = xss_escape(req_json.get("userId"))
-#     username = xss_escape(req_json.get('username'))
-#     real_name = xss_escape(req_json.get('realName'))
-#     dept_id = xss_escape(req_json.get('deptId'))
-#     role_ids = a.split(',')
-#     User.query.filter_by(id=id).update({'username': username, 'realname': real_name, 'dept_id': dept_id})
-#     u = User.query.filter_by(id=id).first()
-#
-#     roles = Role.query.filter(Role.id.in_(role_ids)).all()
-#     u.role = roles
-#
-#     db.session.commit()
-#     return success_api(msg="更新成功")
-#
-#
-#
-#
+
+
+#  敏感词分类编辑
+@admin_wordkinds.get('/edit/<int:id>')
+@authorize("admin:wordkinds:edit", log=True)
+def edit(id):
+    wordkinds_id = get_one_by_id(model=Wordkinds, id=id)
+    return render_template('admin/wordkinds/edit.html', wordkinds=wordkinds_id)
+
+
+#  更新分类
+@admin_wordkinds.put('/update')
+@authorize("admin:wordkinds:edit", log=True)
+def update():
+    req_json = request.json
+    id = req_json.get("id")
+    data = {
+        "kindname": xss_escape(req_json.get("kindname")),
+        "sort": xss_escape(req_json.get("sort")),
+        "enable": xss_escape(req_json.get("enable")),
+        "remark": xss_escape(req_json.get("remark"))
+    }
+    wordkinds = Wordkinds.query.filter_by(id=id).update(data)
+    db.session.commit()
+    if not wordkinds:
+        return fail_api(msg="更新敏感词分类失败")
+    return success_api(msg="更新敏感词分类成功")
+
+
+
+
 #
 #
 #
@@ -139,34 +135,34 @@ def save():
 #         return fail_api(msg="出错啦")
 #     return success_api(msg="更新成功")
 #
-#
-#
-# # 启用用户
-# @admin_wordkinds.put('/enable')
-# @authorize("admin:user:edit", log=True)
-# def enable():
-#     _id = request.json.get('userId')
-#     if _id:
-#         res = enable_status(model=User, id=_id)
-#         if not res:
-#             return fail_api(msg="出错啦")
-#         return success_api(msg="启动成功")
-#     return fail_api(msg="数据错误")
-#
-#
-# # 禁用用户
-# @admin_wordkinds.put('/disable')
-# @authorize("admin:user:edit", log=True)
-# def dis_enable():
-#     _id = request.json.get('userId')
-#     if _id:
-#         res = disable_status(model=User, id=_id)
-#         if not res:
-#             return fail_api(msg="出错啦")
-#         return success_api(msg="禁用成功")
-#     return fail_api(msg="数据错误")
-#
-#
+
+
+# 启用用户
+@admin_wordkinds.put('/enable')
+@authorize("admin:wordkinds:edit", log=True)
+def enable():
+    _id = request.json.get('Id')
+    if _id:
+        res = enable_status(model=Wordkinds, id=_id)
+        if not res:
+            return fail_api(msg="出错啦")
+        return success_api(msg="启用成功")
+    return fail_api(msg="启用失败")
+
+
+# 禁用用户
+@admin_wordkinds.put('/disable')
+@authorize("admin:wordkinds:edit", log=True)
+def dis_enable():
+    _id = request.json.get('Id')
+    if _id:
+        res = disable_status(model=Wordkinds, id=_id)
+        if not res:
+            return fail_api(msg="出错啦")
+        return success_api(msg="禁用成功")
+    return fail_api(msg="禁用失败")
+
+
 # # 批量删除
 # @admin_wordkinds.delete('/batchRemove')
 # @authorize("admin:user:remove", log=True)
